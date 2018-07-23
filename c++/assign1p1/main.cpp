5f2a7d0d7a405af2443cd428c20b080c96cc6a92
francisknight <dfranc15@my.chemeketa.edu>
2018-07-23 11:24:55 -0700

    Update main.cpp

--- c++/assign1p1/main.cpp
+++ c++/assign1p1/main.cpp
@@ -24,6 +24,18 @@ int main()
 
     fourDigitInt /= 10;
     cout << fourDigitInt % 10 << endl;
+ // --------------------------------------------------------------
+     //Cleaner way -- Get input from user
+    int fourDigitNum;
+    cout << "Enter a four digit integer: ";
+    cin >> fourDigitNum;
+
+    //Display result
+    cout << fourDigitNum % 10 << endl;
+    for (int i = 0; i < 3; i++ ) {
+        fourDigitNum /= 10;
+        cout << fourDigitNum % 10 << endl;
+    }
 
     return 0;
 }
