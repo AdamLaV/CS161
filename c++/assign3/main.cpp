af5c08bba7987faf4f34e6135507c5201332de48
francisknight <dfranc15@my.chemeketa.edu>
2018-07-31 11:45:31 -0700

    Week 3 HW

---
+++ c++/assign3/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    // Get inout from user
+    const double PI = 3.14259;
+    int a, b, A;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+    cout << "Enter A: ";
+    cin >> A;
+
+
+    return 0;
+}
