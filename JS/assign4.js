987def65567e0dadf135f0b74d109b07477085d2
francisknight <dfranc15@my.chemeketa.edu>
2018-08-05 14:00:38 -0700

    assign4 in JavaScript

---
+++ JS/assign4.js
@@ -0,0 +1,14 @@
+let balance = 1000;
+let payment = 150;
+let APR = 19.9, interestRate = 0;
+let month = 0;
+
+// Compute output
+console.log('Month  ' + ' int    ' + ' Pay ' + ' balance ');
+
+while (interestRate >= 0) {
+	console.log(month + '      ' + interestRate.toPrecision(3) + '    ' + payment + '    ' + balance.toPrecision(4));
+	month++
+	interestRate = (APR / 100) / 12 * balance;
+	balance = balance - payment + interestRate;
+}
