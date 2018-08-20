c3e8d4af3142938fc3c6e9d5d20276e779299f36
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-08-20 09:26:38 -0700

    final files

--- c++/assign5/assign5.cpp
+++ c++/assign5/assign5.cpp
@@ -8,22 +8,19 @@ int main()
     ifstream input("StudentData.txt");
 
     //Read data
-    string firstName, lastName, email, username;
+    string firstName, lastName, email;
     double score1, score2, score3, score4, score5, score6, score7, score8, sum = 0;
-    int index = 0;
 
     while (input >> firstName >> lastName >> email >> score1 >> score2 >> score3 >> score4 >>
            score5 >> score6 >> score7 >> score8) {
         if(input.eof()) break;
-        index = email.find("@");
-        username = email.substr(0, index);
+        int index = email.find("@");
+        string username = email.substr(0, index);
         sum = score1 + score2 + score3 + score4 + score5 + score6 + score7 + score8;
-        cout << firstName << " " << lastName <<" " << username << " " << fixed << setprecision(1) << sum / 8 << endl;
+        cout << username << " " << fixed << setprecision(1) << sum / 8 << endl;
     }
 
     input.close();
-
-    cout << "Done!" << endl;
 
     return 0;
 }
