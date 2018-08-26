93371e61878fb2cdd8abdcef2efc91e57e07197a
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-08-26 13:01:01 -0700

    added a second function

--- c++/assign6.cpp
+++ c++/assign6.cpp
@@ -5,14 +5,15 @@
 using namespace std;
 
 string getInput(const string& prompt);
-
+int strToInt(string digit);
 int main()
 {
   //  int b = atoi(cardNumber.c_str());
    // cout << "You Entered " << b + 2 << endl;
 
     string cardnumber = getInput("Enter Card Number: ");
-    cout << cardnumber;
+    int cardDigits = strToInt(cardnumber);
+    cout << cardDigits << endl;
 
     return 0;
 }
@@ -23,4 +24,10 @@ string getInput(const string& prompt) {
     string cardNumber;
     cin >> cardNumber;
     return cardNumber;
+}
+
+// Turns a string like '3333' into the number it represents (3333)
+int strToInt(string digits) {
+    int stringDigit = atoi(digits.c_str());
+    return stringDigit;
 }
