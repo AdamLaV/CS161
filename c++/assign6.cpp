ab27a1ab891338e65f18449bc6c2935052bb1d2b
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-08-27 16:39:25 -0700

    added a third function: getCardType

--- c++/assign6.cpp
+++ c++/assign6.cpp
@@ -5,15 +5,16 @@
 using namespace std;
 
 string getInput(const string& prompt);
+string getCardType(const string& cardNumber);
 int strToInt(string digit);
 int main()
 {
-  //  int b = atoi(cardNumber.c_str());
-   // cout << "You Entered " << b + 2 << endl;
 
     string cardnumber = getInput("Enter Card Number: ");
-    int cardDigits = strToInt(cardnumber);
-    cout << cardDigits << endl;
+   // int cardDigits = strToInt(cardnumber);
+    //cout << cardDigits << endl;
+    string cardnumberType = getCardType(cardnumber);
+    cout << cardnumberType << endl;
 
     return 0;
 }
@@ -26,8 +27,27 @@ string getInput(const string& prompt) {
     return cardNumber;
 }
 
+// Return a string representing the type of crdit card a number is: VISA, MASTERCARD, AMERICAN EXPRESS, OR UNKNOWN
+string getCardType(const string& cardNumber) {
+    string cardType = "";
+    if(cardNumber.at(0) == '4') {
+        cardType = "VISA";
+    }
+    else if (cardNumber.at(0) == '5') {
+        cardType = "MASTERCARD";
+    }
+    else if (cardNumber.at(1) == '4' || (cardNumber.at(1) == '7' && cardNumber.at(0) == '3')) {
+        cardType = "AMERICAN EXPRESS";
+    }
+    else {
+        cardType = "UNKNOWN";
+    }
+    return cardType;
+}
+
 // Turns a string like '3333' into the number it represents (3333)
 int strToInt(string digits) {
     int stringDigit = atoi(digits.c_str());
     return stringDigit;
 }
+
