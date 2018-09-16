d2f928e6205b11d2e7cbcb46486bbd6b6a942619
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-09-16 15:51:20 -0700

    Update assign7functions.cpp

--- c++/assign7functions.cpp
+++ c++/assign7functions.cpp
@@ -1,64 +1,48 @@
-// avearge function
+//average function
 double average(const int scores[], int size) {
-	double average = 0.0;
-	for (int i = 0; i < size; i++)
-	{
-		average += scores[i]; 
-	}
+    double average = 0.0;
+    for (int i = 0; i < size; i++)
+    {
+        average += scores[i];
+    }
 
-	return average / size;
+    return average / size;
 }
 
-// standard deviation function 
+// standard deviation function
 double standardDeviation(const int scores[], int size, double mean) {
-	double roundedStandardDev = 0.0, sqrtStandardDev = 0.0;
-	for (int i = 0; i < size; ++i)
-	{
-		roundedStandardDev += pow(scores[i] - mean, 2);
-		sqrtStandardDev = sqrt(roundedStandardD / 10);
-	}
+    double roundedStandardDev = 0.0, sqrtStandardDev = 0.0;
+    for (int i = 0; i < size; ++i)
+    {
+        roundedStandardDev += pow(scores[i] - mean, 2);
+        sqrtStandardDev = sqrt(roundedStandardDev / 10);
+    }
 
-	return sqrtStandardDev;
+    return sqrtStandardDev;
 }
 
 // getLetterGrade function
 char getLetterGrade(const int score, double mean, double stdDev) {
-	char greade;
-	double stdDivid = (scroe / stdDev);
+    char grade;
 
-	// below average conditions
-	if (score < mean)
-	{
-		if (stdDivid > 1)
-		{
-			greade = "F";
-		}
-
-		else if (stdDivid >= 0.5)
-		{
-			greade = "D";
-		}
-
-		else if (stdDivid < 0.5)
-		{
-			greade = "C";
-		}
-	}
-
-	// above avearge conditions 
-	else if (score > mean)
-	{
-		if (stdDivid > 0.5)
-		{
-			greade = "B";
-		}
-
-		else if (stdDivid > 1)
-		{
-			greade = "A";
-		}
-	}
-
-	return grade;
+    // below average students
+    double stdDivid = (mean - score);
+    if(score < mean) {
+        //stdDivid = (mean - score);
+        if(stdDivid / stdDev >= 1.0) {
+            grade = 'F';
+        }
+        else if(stdDivid / stdDev >= 0.5) {
+            grade = 'D';
+        }
+        else {
+            grade = 'C';
+        }
+    }
+    if(score > mean) {
+        grade = 'B';
+    }
+    return grade;
+    //mean = mean * stdDev + score;
 
 }
