b66e3f0d8d2fa7fb33563add85606a5f2c5c6af2
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-09-18 18:50:39 -0700

    starter files

--- c++/assign6.cpp
+++ c++/assign6.cpp
@@ -7,32 +7,17 @@ using namespace std;
 
 string getInput(const string& prompt);
 string getCardType(const string& cardNumber);
+int sumOfDigits(const string& cardNumber);
 int charToInt(char digit);
-bool isValid(const string& cardNumber);
-int sumOfDigits(const string& CardNumber);
-int doubleDigitValue(int number);
 
 int main()
 {
-
-    string cardnumber = getInput("Enter Card Number: ");
-    string cardnumberType = getCardType(cardnumber);
-    bool cardNumberValid = isValid(cardnumber);
-    if(cardNumberValid == true) {
-        cout << "Valid Number, " << cardnumberType << endl;
-    }
-    else {
-        cout << "Not a Valid Number, " << cardnumberType << endl;
-    }
-
-    int num = sumOfDigits(cardnumber);
-    cout << num + 3 << endl;
+    string card = getInput("Enter Card Number: ");
+    string cardType = getCardType(card);
+    cout << card << " " << cardType << endl;
 
     return 0;
 }
-
-
-
 
 // Takes in a string, prints it then get input and returns the input
 string getInput(const string& prompt) {
@@ -60,40 +45,21 @@ string getCardType(const string& cardNumber) {
     return cardType;
 }
 
-// Returns true/false indicating if a credit card number is potentially a valid number...
-bool isValid(const string& cardNumber) {
-    bool validNumber = false;
-    if(cardNumber.at(0) == '1') {
-        validNumber = true;
-    }
+// sum of digit
+int sumOfDigits(const string& cardNumber) {
+    int total_num = cardNumber.length();
+    int card[total_num];
+    int sum = 0;
 
-    return validNumber;
+    for(int i = 0; i < total_num; i++) {
+        card[i] = charToInt(cardNumber[i]);
+        sum += card[i];
+    }
+    return sum;
 }
 
-// Turns a string like '3333' into the number it represents (3333)
+// charToInt
 int charToInt(char digit) {
-    int charDigit = digit-'0';
-    return charDigit;
+    int digitValue = digit - '0';
+    return digitValue;
 }
-
-// Sums the digits of a credit card number
-int sumOfDigits(const string& CardNumber) {
-    int cardDigits, sum = 0, evenSum = 0, oddSum = 0;
-    int myArray[16];
-
-    // loop backwards through cardNumber
-    for(int i = CardNumber.length() - 1; i >= 0; i--) {
-        cardDigits = charToInt(CardNumber.at(i));
-        myArray[i] = cardDigits;
-    }
-
-    return cardDigits;
-}
-
-// Process a number to be doubled
-int doubleDigitValue(int number) {
-    int myArray[] = number;
-    return myArray;
-
-}
-
