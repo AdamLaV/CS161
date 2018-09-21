b46475a4fdcc3ac646b2204049907acc6432c394
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-09-21 16:58:36 -0700

    Update assign1p1.cpp

--- c++/Fall 2018/Distance Finder/assign1p1.cpp
+++ c++/Fall 2018/Distance Finder/assign1p1.cpp
@@ -23,7 +23,7 @@ int main()
     return 0;
 }
 
-// distnace finder function
+// distance finder function
 void DistanceFinder(int inches) {
     const double mi = 0.000015783, yd = 0.027778, ft = 0.083333;
     double miles = inches * mi;
