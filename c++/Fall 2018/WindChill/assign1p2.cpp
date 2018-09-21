b1da31155ba8a1398aba24d6795cb36cf046dfd2
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-09-20 18:32:47 -0700

    Update assign1p2.cpp

--- c++/Fall 2018/WindChill/assign1p2.cpp
+++ c++/Fall 2018/WindChill/assign1p2.cpp
@@ -3,19 +3,23 @@
   * @author Francis
   */
 #include <iostream>
-#include <cmath>
+#include <math.h>
 using namespace std;
 
 int main()
 {
-    double temperature;
+    double temperature, windChill;
     int windSpeed;
-    
-    // get user input 
-    cout <<"Enter the temperature in Fahrenheit: ";
+
+    // get user input
+    cout << "Enter the temperature in Fahrenheit: ";
     cin >> temperature;
     cout << "Enter the windchill in MPH: ";
     cin >> windSpeed;
+
+    // compute windchill
+    windChill = 35.74 + (0.6125 * temperature) - 35.75 * pow(windSpeed, 0.16) + (0.4275 * temperature) * pow(windSpeed, 0.16);
+    cout << "The wind chill index is " << windChill << endl;
 
     return 0;
 }
