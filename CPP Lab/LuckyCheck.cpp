7f4e097234b755adacf9d38b1b0d31f7fd7be4f1
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-10-01 15:05:33 -0700

    Create LuckyCheck.cpp

---
+++ CPP Lab/LuckyCheck.cpp
@@ -0,0 +1,15 @@
+// Read in a single integer from cin. If it is 7, output "Lucky". If it is not 7, output nothing.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int luckyNum;
+	cin >> luckyNum;
+		
+	if (luckyNum == 7) {
+		cout << "Lucky";
+	}	
+	return 0;
+}
