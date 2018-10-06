9e6dc02a7e68b88e73f16417167ab5f04b5ebb3d
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-10-06 15:02:49 -0700

    Update sssSolver.cpp

--- c++/Fall 2018/SSS Solver/sssSolver.cpp
+++ c++/Fall 2018/SSS Solver/sssSolver.cpp
@@ -21,15 +21,35 @@ int main()
 
     // compute the three angles
     double angle1, angle2, angle3;
-    double const angleInRadians = 180 / 3.1415927;
+    double const angleInRadians = 180 / 3.1415927; // value of pi = 3.1415927
     angle1 = acos((length1 * length1 - length2 * length2 - length3 * length3) / (-2 * length2 *length3));
     angle2 = acos((length2 * length2 - length1 * length1 - length3 * length3) / (-2 * length1 *length3));
     angle3 = acos((length3 * length3 - length2 * length2 - length1 * length1) / (-2 * length1 *length2));
 
+    // classifies the triangle
+    double SumOfAngle = (angle1 + angle2 + angle3) * angleInRadians;
+    string AngleType;
+
+    // Acute angle
+    if (SumOfAngle > 0 && SumOfAngle < 90 ) {
+        AngleType = "Acute";
+    }
+    // Obtuse angle
+    else if (SumOfAngle > 90 && SumOfAngle < 180) {
+        AngleType = "Obtuse";
+    }
+    // Right angle
+    else if (SumOfAngle == 90) {
+        AngleType = "Right";
+    }
+    else {
+        AngleType = "Impossible Triangle";
+    }
+
+
     cout << fixed << setprecision(4) << "Angle 1: " << angle1 * angleInRadians << "\n"
          << "Angle 2: " << angle2 * angleInRadians << "\n"
-         << "Angle 3: " << angle3 * angleInRadians <<endl;
-
+         << "Angle 3: " << angle3 * angleInRadians << "\n" << AngleType << endl;
 
     return 0;
 }
