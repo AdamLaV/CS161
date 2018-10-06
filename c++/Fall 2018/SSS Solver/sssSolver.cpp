0875972bfdafb8238c2753c56c53e18a8e74b2e5
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-10-06 12:42:12 -0700

    Update sssSolver.cpp

--- c++/Fall 2018/SSS Solver/sssSolver.cpp
+++ c++/Fall 2018/SSS Solver/sssSolver.cpp
@@ -1,8 +1,25 @@
-#include <iostream>
+/**
+  * @brief Assignment 3
+  * @author Daidemang Francis
+  */
 
+#include <iostream>
+#include <cmath>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    // get lengths of triangle from user
+    double length1, length2, length3;
+    cout << "Enter length 1: ";
+    cin >> length1;
+    cout << "Enter length 2: ";
+    cin >> length2;
+    cout << "Enter length 3: ";
+    cin >> length3;
+    
+    // compute the three angles 
+
+    return 0;
 }
