2bb8b79eefc644e46282d3c82c9b2530c7a56908
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-10-06 13:11:34 -0700

    Update sssSolver.cpp

--- c++/Fall 2018/SSS Solver/sssSolver.cpp
+++ c++/Fall 2018/SSS Solver/sssSolver.cpp
@@ -18,8 +18,18 @@ int main()
     cin >> length2;
     cout << "Enter length 3: ";
     cin >> length3;
-    
-    // compute the three angles 
+
+    // compute the three angles
+    double angle1, angle2, angle3;
+    double const angleInRadians = 180 / 3.1415927;
+    angle1 = acos((length1 * length1 - length2 * length2 - length3 * length3) / (-2 * length2 *length3));
+    angle2 = acos((length2 * length2 - length1 * length1 - length3 * length3) / (-2 * length1 *length3));
+    angle3 = acos((length3 * length3 - length2 * length2 - length1 * length1) / (-2 * length1 *length2));
+
+    cout << fixed << setprecision(4) << "Angle 1: " << angle1 * angleInRadians << "\n"
+         << "Angle 2: " << angle2 * angleInRadians << "\n"
+         << "Angle 3: " << angle3 * angleInRadians <<endl;
+
 
     return 0;
 }
