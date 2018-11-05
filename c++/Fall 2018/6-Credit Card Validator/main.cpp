2bfb9e84e71c660d58077e4a8cf46d708ce4495e
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-11-05 13:13:35 -0800

    Update main.cpp

--- c++/Fall 2018/6-Credit Card Validator/main.cpp
+++ c++/Fall 2018/6-Credit Card Validator/main.cpp
@@ -36,7 +36,7 @@ int main()
 // ----------------------- end testing ------------------ \\
 
 
-// ----------- Final Program ------------- \\
+// ----------- Final Program Part B------------- \\
 
     // Get user input
     string cardNumber;
