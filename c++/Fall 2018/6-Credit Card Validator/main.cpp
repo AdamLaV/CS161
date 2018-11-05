f7b885aa2a58830d3dad2aebd92fbb4a9ee9f20c
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-11-05 12:31:04 -0800

    Create main.cpp

---
+++ c++/Fall 2018/6-Credit Card Validator/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "cradFunctions.h"
+#include <string>
+using namespace std;
+
+int main()
+{
+
+// ----------- Function building + testing Part A ------------- \\
+//      string test1 = getCardType(cardNumber);
+//      cout << test1;
+//      bool status = isValid(cardNumber);
+//      cout << status;
+
+//      string test2 = luhnDigitSum(cardNumber);
+//      cout << test2 << endl;
+
+//    int test3 = luhnDigitSum(cardNumber);
+//    cout << test3 << endl;
+//    cout << "Enter charValue: ";
+//    cin >> test4p1;
+//    cout << "Enter charValue: ";
+//    cin >> test4p2;
+//    char test4p1 = '3', test4p2 = '3';
+//    int test4Total = 0;
+//    test4Total += charToInt(test4p1);
+//    test4Total += charToInt(test4p2);
+//    cout << test4Total << endl;
+
+//    int testResult = doubleDigitValue(4);
+//    cout << testResult << endl;
+// ----------------------- end testing ------------------ \\
+
+
+// ----------- Final Program ------------- \\
+
+    // Get user input
+    string cardNumber;
+    cout << "Enter card number: ";
+    cin >> cardNumber;
+
+    // validate card number
+    string cardType = getCardType(cardNumber);
+    bool status = isValid(cardNumber);
+
+    if(status == 1) {
+        cout << "Valid number, " << cardType << endl;
+    }
+
+
+    return 0;
+}
