58d5b6db8c8f7b4a1445d10c0fbdecee58494b34
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-11-10 21:20:05 -0800

    Create main.cpp

---
+++ c++/Fall 2018/Climb Stats/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include "hikeFunctions.h"
+using namespace std;
+
+int main()
+{
+    const int HIKE_LENGTH = 9;
+    int hikeData[HIKE_LENGTH] = {1200, 3000, 3450, 2800, 2900, 1550, 1750, 1110, 1200};
+
+    // get hightest points from hikeData
+    int firstHalf = getHighestPointBetween(hikeData, 0, 4);
+    cout << firstHalf << endl;
+
+    return 0;
+}
