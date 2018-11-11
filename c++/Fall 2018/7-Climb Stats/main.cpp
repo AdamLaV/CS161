757ef2f77a0c87b76b15f310143f403e485637f5
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-11-10 21:45:27 -0800

    Update main.cpp

--- c++/Fall 2018/7-Climb Stats/main.cpp
+++ c++/Fall 2018/7-Climb Stats/main.cpp
@@ -4,6 +4,7 @@
 */
 #include <iostream>
 #include <cmath>
+#include <array>
 #include "hikeFunctions.h"
 using namespace std;
 
@@ -11,11 +12,12 @@ int main()
 {
     const int HIKE_LENGTH = 9;
     int hikeData[HIKE_LENGTH] = {1200, 3000, 3450, 2800, 2900, 1550, 1750, 1110, 1200};
+    //const int LAST_INDEX  = hikeData.back();
 
     // get hightest points from hikeData
-    int firstHalf = getHighestPointBetween(hikeData, 0, 4);
-    cout << firstHalf << endl;
+    int firstHalf = getHighestPointBetween(hikeData, 0, 5);
+    int secondHalf = getHighestPointBetween(hikeData, 5, 8);
+    cout << firstHalf << " " << secondHalf << endl;
 
     return 0;
 }
-
