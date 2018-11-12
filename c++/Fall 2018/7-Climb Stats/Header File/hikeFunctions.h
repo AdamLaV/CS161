42061627413b8f04787e7326d6c3ab5d288f9886
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-11-12 13:22:08 -0800

    Update hikeFunctions.h

--- c++/Fall 2018/7-Climb Stats/Header File/hikeFunctions.h
+++ c++/Fall 2018/7-Climb Stats/Header File/hikeFunctions.h
@@ -38,11 +38,10 @@ int getNumSteepSegments(const int heights[], int size);
 
 /**
  * @brief getTotalChnage compute the total chnage covered the hike (uphill or downhill)
- * @param heigths array with hiking data 
+ * @param heigths array with hiking data
  * @param startMile starting position
  * @param endMile ending position
  * @return the total elevetion chnage over the range from startMile to endMile
  */
-int getTotalChnage(const int heigths[], int startMile, int endMile);
+int getTotalChnage(const int heights[], int startMile, int endMile);
 #endif // HIKEFUNCTIONS_H
-
