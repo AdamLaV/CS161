713c600c8dc4ddc48b94b5fe3a3b73a75c6b9c2c
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-11-30 20:25:57 -0800

    Update main.cpp

--- c++/Fall 2018/PhotoChop/Functions/main.cpp
+++ c++/Fall 2018/PhotoChop/Functions/main.cpp
@@ -12,3 +12,22 @@ void onlyRed(Image& source) {
     }
 }
 
+
+/**
+ * @brief rotateLeft rotate an image left
+ * @param source Image to be rotated
+ */
+void rotateLeft(Image& source) {
+    Image newImage;
+
+    for(int i = 0; i < IMG_HEIGHT; i++) {
+        for(int j = 0; j < IMG_WIDTH; j++) {
+            int newRow = i;
+            int newCol = (IMG_WIDTH - 1) - j;
+
+            newImage.data[newRow][newCol] = source.data[i][j];
+        }
+    }
+
+    source = newImage;
+}
