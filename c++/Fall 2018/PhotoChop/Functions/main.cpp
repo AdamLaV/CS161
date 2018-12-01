742f665159551adfe44f479f8805eaf1f3dde170
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-11-30 20:25:32 -0800

    Create main.cpp

---
+++ c++/Fall 2018/PhotoChop/Functions/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @brief onlyRed set the blue and green value for each picture to black
+ * @param source Image to modify
+ */
+void onlyRed(Image& source) {
+    for(int i = 0; i < IMG_HEIGHT; i++) {
+        for(int j = 0; j < IMG_WIDTH; j++) {
+            source.data[i][j].blue = clamp(source.data[i][j].blue = 0);
+            source.data[i][j].green = clamp(source.data[i][j].green = 0);
+
+        }
+    }
+}
+
